1ab2e4a738828702ef377c0a5e3533dbc7485762
amll-ctrl <amalfarhan50@gmail.com>
2026-08-18 22:14:18 +0530

    Fix missing UserRole import in admin login

--- lib/screens/auth/admin_login_screen.dart
+++ lib/screens/auth/admin_login_screen.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
+import '../../models/user_profile.dart';
 import '../../providers/request_provider.dart';
 import '../admin/admin_dashboard.dart';
 
